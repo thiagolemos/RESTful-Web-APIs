642793aad6295796dff54c5630295614b6cd7db7
Thiago Lemos <thiago.lemos@prodap.com.br>
2018-08-23 15:09:18 -0300

    Final

--- lib/app.ts
+++ lib/app.ts
@@ -1,18 +1,21 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from './routes/crmRoutes';
+import * as mongoose from "mongoose";
 
 
 class App {
 
     public app: express.Application;
     public routePrv: Routes = new Routes();
+    public mongoUrl: string = 'mongodb://localhost/CRMbd';
 
     constructor() {
-        
+
         this.app = express();
         this.config();
         this.routePrv.routes(this.app);
+        this.mongoSetup();
 
     }
 
@@ -22,7 +25,12 @@ class App {
         this.app.use(bodyParser.json());
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
-        
+
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 
 }
--- lib/routes/crmRoutes.ts
+++ lib/routes/crmRoutes.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { ContactController } from '../controllers/crmController';
+import { NextFunction } from "express-serve-static-core";
 
 
 export class Routes {
@@ -19,12 +20,16 @@ export class Routes {
         //Contact
         app.route('/contact')
             //Get endpoint
-            .get(this.contactController.getContacts)
+            .get((req: Request, res: Response, next: NextFunction) => {
+                //middleware
+                if (req.query.key !== '9f4da15fd9ac5ba8e52ab6f14c18e69c60ba6d5b') {
+                    res.status(401).send('You shall not pass!');
+                } else {
+                    next();
+                }
+            }, this.contactController.getContacts)
             //POST endpoint
-            .post((req: Request, res: Response) => {
-                //Create new contact
-                res.status(200).send(this.contactController.addNewContact);
-            })
+            .post(this.contactController.addNewContact)
 
         //Contact detail
         app.route('/contact/:contactId')
